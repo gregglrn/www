8a37e53b286a20f984564e7902f90e9285a8e6ce
gregglrn <gillieron.gregory2@gmail.com>
2023-12-06 10:30:43 +0100

    update_db

--- Gregory/BD/BD_shema.sql
+++ Gregory/BD/BD_shema.sql
@@ -14,9 +14,44 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de la base pour epfl_timbreuse
+CREATE DATABASE IF NOT EXISTS `epfl_timbreuse` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `epfl_timbreuse`;
+
+-- Listage de la structure de table epfl_timbreuse. t_heure
+CREATE TABLE IF NOT EXISTS `t_heure` (
+  `id_heure` int NOT NULL AUTO_INCREMENT,
+  `id_personne` int DEFAULT NULL,
+  `date_heure` datetime DEFAULT NULL,
+  PRIMARY KEY (`id_heure`),
+  KEY `id_personne` (`id_personne`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table epfl_timbreuse. t_heure_personne
+CREATE TABLE IF NOT EXISTS `t_heure_personne` (
+  `id_heure_personne` int NOT NULL AUTO_INCREMENT,
+  `fk_heure_personne` int DEFAULT NULL,
+  PRIMARY KEY (`id_heure_personne`),
+  KEY `fk_heure_personne` (`fk_heure_personne`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Les données exportées n'étaient pas sélectionnées.
+
+-- Listage de la structure de table epfl_timbreuse. t_personne
+CREATE TABLE IF NOT EXISTS `t_personne` (
+  `id_personne` int NOT NULL AUTO_INCREMENT,
+  `prenom_personne` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `nom_personne` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `gaspar_personne` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `mail_personne` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `password_personne` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `role_personne` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'Apprentis',
+  `heure_par_jour` time DEFAULT '08:12:00',
+  PRIMARY KEY (`id_personne`)
+) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Les données exportées n'étaient pas sélectionnées.
 
